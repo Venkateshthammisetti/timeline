d6e01fc6451fce64fd71e804f9ce10ce50adfc95
Venkateshthammisetti <91789700+Venkateshthammisetti@users.noreply.github.com>
2023-12-21 10:57:16 +0530

    Add files via upload

---
+++ src/timeLineElement.js
@@ -0,0 +1,48 @@
+let timelineElements = [
+  {
+    id: 3,
+    title: "Frontend Developer Intern",
+    location: "WFH, Chennai office",
+    description:
+      "Converting data to a graphical interface, through the use of HTML, CSS, and JavaScript, so that users can view and interact with that data.",
+    buttonText: "View Frontend Projects",
+    date: "January 2023 - April 2023",
+    icon: "work",
+    link: "https://github.com/Venkateshthammisetti?tab=repositories",
+  },
+  {
+    id: 1,
+    title: "Academic Btech Student",
+    location: "Ongole",
+    description: "I have done my graduation in IIIT RGUKT ONGOLE university.",
+    buttonText: "Btech",
+    date: "June 2019 - May 2023",
+    icon: "school",
+    link: "https://www.linkedin.com/in/venkatesh-thammisetti/",
+  },
+
+  {
+    id: 2,
+    title: "Nxtwave Academic Student",
+    location: "online (collage)",
+    description:
+      "Online Course in Fullstack development course and industry ready programm ",
+    buttonText: "Course Certificate",
+    date: "December 2021-Present",
+    icon: "school",
+    link: "https://learning.ccbp.in/",
+  },
+  {
+    id: 4,
+    title: "Professional Artist",
+    location: "Rajahmundry",
+    description:
+      "With interest and skill i have worked as an Professional artist in Strocurve.",
+    buttonText: "Art Projects",
+    date: "June 2023-November 2023",
+    icon: "work",
+    link: "https://github.com/Venkateshthammisetti?tab=repositories",
+  },
+];
+
+export default timelineElements;
